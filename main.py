537f07dfc7228609a41f9b0eb6c72d70887b6498
jdbernardes <juliocd.bernardes@gmail.com>
2024-03-01 21:02:08 -0300

    testing git commit 3

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ print(fila_test_2.chama_cliente(10))
 print(fila_test_2.chama_cliente(5))
 
 
-print(fila_test_2.estatistica('10/01/1993', 198, 'detail'))
+print(fila_test_2.estatistica('10/01/1993', 215, 'detail'))

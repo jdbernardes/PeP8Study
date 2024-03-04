dbe1923954bd8e737d83761660e28da78076b2b1
jdbernardes <juliocd.bernardes@gmail.com>
2024-03-04 18:54:10 -0300

    adicionando injection

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -15,15 +15,7 @@ class FilaPrioritaria(FilaBase):
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente Atual: {cliente_atual}, dirija-se ao caixa {caixa}')
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atentidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = (
-                len(self.clientes_atendidos)
-            )
-        return estatistica
+    def estatistica(self, dia: str, agencia: int, retorna_estatistica) -> dict:
+        estatistica = retorna_estatistica(dia, agencia)
+
+        return estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # from fila_normal import FilaNormal
 # from fila_prioritaria import FilaPrioritaria
 from fabrica_fila import FabricaFila
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
 
 
 # fila_test = FilaNormal()
@@ -19,7 +21,7 @@ from fabrica_fila import FabricaFila
 # print(fila_test_2.chama_cliente(5))
 # print(fila_test_2.estatistica('10/01/1993', 215, 'detail'))
 
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
@@ -27,3 +29,4 @@ teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(12))
 print(teste_fabrica.chama_cliente(10))
 print(teste_fabrica.chama_cliente(2))
+print(teste_fabrica.estatistica('20/03/2025', 120, EstatisticaDetalhada))

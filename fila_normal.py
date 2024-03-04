07caa436dea7b8b096422d9ff103ac30035dfbc5
jdbernardes <juliocd.bernardes@gmail.com>
2024-03-04 16:20:53 -0300

    criando fila base

--- fila_normal.py
+++ fila_normal.py
@@ -1,25 +1,21 @@
-class FilaNormal:
-    codigo:int = 0
-    fila = []
-    clientesatendidos = []
-    senhatual:str = ""
+from fila_base import FilaBase
 
-    def gerasenhaatual(self)->None:
-        self.senhatual = f'NM{self.codigo}'
 
-    #Tipagem de métodos, eu utilizo "->" e informo se ele retorna alguma info, se for None, significa que não tem return, ele apenas executa algo internamente
-    def resetafila(self)->None:
-        if self.codigo>=100:
-            self.codigo=0
-        else:
-            self.codigo+=1
+class FilaNormal(FilaBase):
+    def gerasenhaatual(self) -> None:
+        self.senha_atual = f'NM{self.codigo}'
 
-    def atualizafila(self)->None:
-        self.resetafila()
+    # Tipagem de métodos:
+    # eu utilizo "->" e informo se ele retorna alguma info,
+    # se for None, significa que não tem return,
+    # ele apenas executa algo internamente
+
+    def atualizafila(self) -> None:
+        self.reseta_fila()
         self.gerasenhaatual()
-        self.fila.append(self.senhatual)
+        self.fila.append(self.senha_atual)
 
-    def chamacliente(self, caixa:int)->str:
-        cliente_atual:str = self.fila.pop()
-        self.clientesatendidos.append(cliente_atual)
-        return(f'Cliente Atual: {cliente_atual}, dirija-se ao caixa {caixa}')
+    def chamacliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop()
+        self.clientes_atendidos.append(cliente_atual)
+        return (f'Cliente Atual: {cliente_atual}, dirija-se ao caixa {caixa}')
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,29 +1,26 @@
-class FilaPrioritaria:
-    codigo:int = 0
+from fila_base import FilaBase
+
+
+class FilaPrioritaria(FilaBase):
+    codigo: int = 0
     fila = []
     clientes_atendidos = []
-    senha_atual:str = ""
+    senha_atual: str = ""
 
-    def gera_senha_atual(self)->None:
+    def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
 
-    def reseta_fila(self)->None:
-        if self.codigo>=100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
-
-    def atualiza_fila(self)->None:
+    def atualiza_fila(self) -> None:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
 
-    def chama_cliente(self, caixa:int)->str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop()
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente Atual: {cliente_atual}, dirija-se ao caixa {caixa}')
 
-    def estatistica(self, dia:str, agencia:int, flag:str)->dict:
+    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
         if flag != 'detail':
             estatistica = {f'{agencia} - {dia}': len(self.clientes_atendidos)}
         else:
@@ -31,5 +28,7 @@ class FilaPrioritaria:
             estatistica['dia'] = dia
             estatistica['agencia'] = agencia
             estatistica['clientes_atentidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = len(self.clientes_atendidos)
+            estatistica['quantidade_clientes_atendidos'] = (
+                len(self.clientes_atendidos)
+            )
         return estatistica
